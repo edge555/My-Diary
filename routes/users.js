6d28c35cf353c43981de84e03ac07d6c0702da25
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-05-19 21:29:00 +0600

    Add register form validation

--- routes/users.js
+++ routes/users.js
@@ -12,3 +12,25 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
+
+// Register form POST
+router.post('/register',(req,res)=>{
+    let errors =[];
+    if(req.body.password!=req.body.password2){
+        errors.push({text:'Password doesnt match'});
+    }
+    if (req.body.password.length < 6) {
+      errors.push({ text: 'Password length must be atleast 6 characters' });
+    }
+    if(errors.length!=0){
+        res.render("users/register", {
+          errors: errors,
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          password2: req.body.password2
+        });
+    }else{
+        res.send('Ok');
+    }
+});
