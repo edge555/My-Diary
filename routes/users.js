00d1b83a066e601f649adfbc9ca497db9ee360f9
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-05-19 22:18:32 +0600

    Register form setup

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+//const passport = require('passport');
 const router = express.Router();
 module.exports = router;
+
+// Load user model
+require('../models/User');
+const User = mongoose.model('users');
 
 // User login
 router.get('/login', (req, res) => {
@@ -31,6 +37,38 @@ router.post('/register',(req,res)=>{
           password2: req.body.password2
         });
     }else{
-        res.send('Ok');
+        User.findOne({email : req.body.email})
+        .then(user =>{
+            if(user){
+                req.flash('error_msg','Email already used');
+                res.redirect('/users/register');
+            }else{
+                const newUser = new User({
+                  name: req.body.name,
+                  email: req.body.email,
+                  password: req.body.password,
+                });
+                bcrypt.genSalt(10, (err, salt) => {
+                  bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) {
+                      throw err;
+                    } else {
+                      newUser.password = hash;
+                      newUser
+                        .save()
+                        .then((user) => {
+                          req.flash("success_msg", "Registration successful");
+                          res.redirect('/users/login');
+                        })
+                        .catch((err) => {
+                          console.log(err);
+                          return;
+                        });
+                    }
+                  });
+                });
+            }
+        });
+        
     }
 });
